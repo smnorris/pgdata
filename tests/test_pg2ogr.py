07eb97e6d844968f3e25d9a802b98fe39dd94275
Simon Norris <snorris@hillcrestgeo.ca>
2017-12-14 22:14:11 -0800

    tweak test

--- tests/test_pg2ogr.py
+++ tests/test_pg2ogr.py
@@ -1,67 +1,70 @@
 import os
+import shutil
 import tempfile
+import unittest
 
 import fiona
 
 from pgdb import connect
 
 
-URL = "postgresql://postgres:postgres@localhost:5432/pgdb"
+URL = 'postgresql://postgres:postgres@localhost:5432/pgdb'
 DB = connect(URL)
 DB.execute('CREATE SCHEMA IF NOT EXISTS pgdb')
 DB1 = connect(URL)
-AIRPORTS = os.path.join(os.path.dirname(__file__), 'data/bc_airports.json')
+
+DATA = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
+AIRPORTS = os.path.join(DATA, 'bc_airports.json')
 
 
-def test_ogr2pg():
-    db = DB1
-    db.ogr2pg(AIRPORTS, in_layer="bc_airports", out_layer='bc_airports',
-              schema='pgdb')
-    airports = db['pgdb.bc_airports']
-    assert 'physical_address' in airports.columns
-    assert sum(1 for _ in airports.all()) == 425
+class ogrpg(unittest.TestCase):
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
 
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
 
-def test_pg2geojson():
-    db = DB1
-    tempdir = tempfile.mkdtemp()
-    db.pg2ogr(sql="SELECT * FROM pgdb.bc_airports LIMIT 10", driver="GeoJSON",
-              outfile=os.path.join(tempdir, "test_dump.json"))
-    c = fiona.open(os.path.join(tempdir, "test_dump.json"), "r")
-    assert len(c) == 10
+    def test_ogr2pg(self):
+        db = DB1
+        db.ogr2pg(AIRPORTS, in_layer='bc_airports', out_layer='bc_airports',
+                  schema='pgdb')
+        airports = db['pgdb.bc_airports']
+        assert 'physical_address' in airports.columns
+        assert sum(1 for _ in airports.all()) == 425
 
+    def test_pg2geojson(self):
+        db = DB1
+        db.pg2ogr(sql='SELECT * FROM pgdb.bc_airports LIMIT 10', driver='GeoJSON',
+                  outfile=os.path.join(self.tempdir, 'test_dump.json'))
+        c = fiona.open(os.path.join(self.tempdir, 'test_dump.json'), 'r')
+        assert len(c) == 10
 
-def test_pg2gpkg():
-    db = DB1
-    tempdir = tempfile.mkdtemp()
-    db.pg2ogr(sql="SELECT * FROM pgdb.bc_airports LIMIT 10", driver="GPKG",
-              outfile=os.path.join(tempdir, "test_dump.gpkg"),
-              outlayer='bc_airports')
-    c = fiona.open(os.path.join(tempdir, "test_dump.gpkg"), "r")
-    assert len(c) == 10
+    def test_pg2gpkg(self):
+        db = DB1
+        db.pg2ogr(sql='SELECT * FROM pgdb.bc_airports LIMIT 10', driver='GPKG',
+                  outfile=os.path.join(self.tempdir,'test_dump.gpkg'),
+                  outlayer='bc_airports')
+        c = fiona.open(os.path.join(self.tempdir,'test_dump.gpkg'), 'r')
+        assert len(c) == 10
 
+    def test_pg2gpkg_update(self):
+        db = DB1
+        db.pg2ogr(sql='SELECT * FROM pgdb.bc_airports LIMIT 10', driver='GPKG',
+                  outfile=os.path.join(self.tempdir, 'test_dump.gpkg'),
+                  outlayer='bc_airports')
+        db.pg2ogr(sql='SELECT * FROM pgdb.bc_airports LIMIT 10', driver='GPKG',
+                  outfile=os.path.join(self.tempdir, 'test_dump.gpkg'),
+                  outlayer='bc_airports_2')
+        layers = fiona.listlayers(os.path.join(self.tempdir, 'test_dump.gpkg'))
+        assert len(layers) == 2
 
-def test_pg2gpkg_update():
-    db = DB1
-    tempdir = tempfile.mkdtemp()
-    db.pg2ogr(sql="SELECT * FROM pgdb.bc_airports LIMIT 10", driver="GPKG",
-              outfile=os.path.join(tempdir, "test_dump.gpkg"),
-              outlayer='bc_airports')
-    db.pg2ogr(sql="SELECT * FROM pgdb.bc_airports LIMIT 10", driver="GPKG",
-              outfile=os.path.join(tempdir, "test_dump.gpkg"),
-              outlayer='bc_airports_2')
-    layers = fiona.listlayers(os.path.join(tempdir, "test_dump.gpkg"))
-    assert len(layers) == 2
-
-
-def test_pg2ogr_append():
-    db = DB1
-    tempdir = tempfile.mkdtemp()
-    db.pg2ogr(sql="SELECT * FROM pgdb.bc_airports LIMIT 10", driver="GPKG",
-              outfile=os.path.join(tempdir, "test_dump.gpkg"),
-              outlayer='bc_airports')
-    db.pg2ogr(sql="SELECT * FROM pgdb.bc_airports LIMIT 10", driver="GPKG",
-              outfile=os.path.join(tempdir, "test_dump.gpkg"),
-              outlayer='bc_airports', append=True)
-    c = fiona.open(os.path.join(tempdir, "test_dump.gpkg"), "r")
-    assert len(c) == 20
+    def test_pg2ogr_append(self):
+        db = DB1
+        db.pg2ogr(sql='SELECT * FROM pgdb.bc_airports LIMIT 10', driver='GPKG',
+                  outfile=os.path.join(self.tempdir, 'test_dump.gpkg'),
+                  outlayer='bc_airports')
+        db.pg2ogr(sql='SELECT * FROM pgdb.bc_airports LIMIT 10', driver='GPKG',
+                  outfile=os.path.join(self.tempdir, 'test_dump.gpkg'),
+                  outlayer='bc_airports', append=True)
+        c = fiona.open(os.path.join(self.tempdir, 'test_dump.gpkg'), 'r')
+        assert len(c) == 20
